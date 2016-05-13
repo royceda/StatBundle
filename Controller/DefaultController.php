e5b0b1799335f1296d099d336119690f52d29160
royceda <root@arii64>
2016-05-13 12:35:01 +0200

    views

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -3,13 +3,58 @@
 namespace Arii\StatBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+
 
 class DefaultController extends Controller
 {
+  /* CHART */
     public function indexAction()
     {
         return $this->render('StatBundle:Default:index.html.twig');
     }
+
+
+    public function boxplotAction()
+    {
+        return $this->render('StatBundle:tempChart:boxChart.html.twig');
+    }
+
+
+    public function piechartAction()
+    {
+        return $this->render('StatBundle:tempChart:pieChart.html.twig');
+    }
+
+
+    public function linechartAction()
+    {
+        return $this->render('StatBundle:tempChart:lineChart.html.twig');
+    }
+
+    /* DATA */
+
+    public function jobsAction(){
+      $response = new Response();
+      $response->headers->set('Content-Type', 'text/xml');
+      return $this->render('StatBundle:tempChart:jobs.xml.twig',array(), $response );
+    }
+
+    public function dataframeAction(){
+      $response = new Response();
+      $response->headers->set('Content-Type', 'text/xml');
+      return $this->render('StatBundle:tempChart:dataframe.xml.twig',array(), $response );
+    }
+
+
+    public function describeAction(){
+      $response = new Response();
+      $response->headers->set('Content-Type', 'text/xml');
+      return $this->render('StatBundle:tempChart:describe.xml.twig',array(), $response );
+    }
+
+
 
     public function index2Action($name)
     {
